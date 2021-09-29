c7278858c72ec533192526c981a7b73fdb1d3cde
David Zabaleta <davidzabaletaf@gmail.com>
2021-09-29 00:35:34 -0500

    Kafka Producer config changed

--- infrastructure/driven-adapters/async-event-bus/src/main/java/co/berako/events/kafkaproducer/KafkaProducerAdapter.java
+++ infrastructure/driven-adapters/async-event-bus/src/main/java/co/berako/events/kafkaproducer/KafkaProducerAdapter.java
@@ -2,13 +2,24 @@ package co.berako.events.kafkaproducer;
 
 import co.berako.model.events.Event;
 import co.berako.model.events.gateways.EventsGateway;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.Future;
+
+import static java.util.Objects.nonNull;
+
 @Slf4j
 @Component
-public class KafkaProducerAdapter extends KafkaProducerTemplate<String, String> implements EventsGateway {
+@RequiredArgsConstructor
+public class KafkaProducerAdapter implements EventsGateway {
+
+    private final KafkaProducer<String, String> kafkaProducer;
 
     @Override
     public <T> Mono<Boolean> emit(Event<T> event) {
@@ -20,6 +31,29 @@ public class KafkaProducerAdapter extends KafkaProducerTemplate<String, String> 
 
     @Override
     public Mono<Boolean> close() {
-        return closeFlushing();
+        return Mono.fromCallable(() -> {
+            try {
+                kafkaProducer.flush();
+                kafkaProducer.close();
+                return Boolean.TRUE;
+            } catch (Exception e) {
+                return Boolean.FALSE;
+            }
+        });
+    }
+
+    private Mono<Future<RecordMetadata>> sendRecord(ProducerRecord<String, String> record) {
+        return Mono.fromCallable(() -> kafkaProducer
+                .send(record, (recordMetadata, e) -> {
+                    if (nonNull(e))
+                        log.error("Error while producing the message: ", e);
+
+                    log.info("Received new metadata. \n"
+                            .concat(String.format("Topic: %s \n", recordMetadata.topic()))
+                            .concat(String.format("Partition: %s \n", recordMetadata.partition()))
+                            .concat(String.format("Offset: %s \n", recordMetadata.offset()))
+                            .concat(String.format("Timestamp: %s \n", recordMetadata.timestamp()))
+                    );
+                }));
     }
 }
---
+++ infrastructure/driven-adapters/async-event-bus/src/main/java/co/berako/events/kafkaproducer/config/kafkaProducerConfig.java
@@ -0,0 +1,40 @@
+package co.berako.events.kafkaproducer.config;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class kafkaProducerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public <K, V> KafkaProducer<K, V> kafkaProducer() {
+        Properties properties = new Properties();
+
+        /*Basic Producer properties*/
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        /*Safe Producer properties*/
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        /*High throughput Producer properties*/
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "10");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+
+        return new KafkaProducer<>(properties);
+    }
+}
